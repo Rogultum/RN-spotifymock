a4d40c9f183e6773a186974b51a0db7c3d9db25f
Remzi Ogul TUM <amadeuwoles@gmail.com>
2022-09-14 01:05:37 +0300

    feat: :package: add "Profile Screen"

---
+++ src/screen/ProfileStack/ProfileScreen/ProfileScreen.js
@@ -0,0 +1,76 @@
+import React, { useEffect, useState } from 'react';
+import { Image, Text, View } from 'react-native';
+
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useNavigation } from '@react-navigation/native';
+import { useSelector } from 'react-redux';
+
+import Input from '../../../component/Input';
+import styles from './ProfileScreen.style';
+
+import { SPOTIFY_GREEN, DARK_THEME, SPOTIFY_BLACK } from '../../../colours/spotifycolours';
+import ProfileButton from '../../../component/Button/ProfileButton/ProfileButton';
+
+function ProfileScreen() {
+    const navigation = useNavigation();
+
+    const theme = useSelector((state) => state.theme.value);
+
+    useEffect(() => {
+        getUser();
+    }, []);
+    const [userImageUrl, setUserImageUrl] = useState();
+    const [user, setUser] = useState({});
+
+    const getUser = async () => {
+        const userData = await AsyncStorage.getItem('user');
+        setUser(JSON.parse(userData));
+    };
+
+    const handleLogout = async () => {
+        setUser(null);
+        await AsyncStorage.removeItem('user');
+        navigation.navigate('SignUp');
+    };
+
+    const navigateEditProfile = () => {
+        navigation.navigate('EditProfile');
+    };
+
+    const navigateTheme = () => {
+        navigation.navigate('Theme');
+    };
+
+    return (
+        <View style={{ flex: 1, backgroundColor: theme ? 'white' : SPOTIFY_BLACK }}>
+            <View style={styles.image_container}>
+                {userImageUrl ? (
+                    <Image style={styles.image} source={{ uri: userImageUrl }} />
+                ) : (
+                    <Input
+                        label="Please provide an image URL for your profile picture."
+                        placeholder="Paste only URL."
+                        onChangeText={setUserImageUrl}
+                    />
+                )}
+                <View style={styles.username_container}>
+                    <Text style={{ color: theme ? 'grey' : 'white' }}>Username:</Text>
+                    {user ? (
+                        <Text style={{ color: theme ? 'grey' : 'white' }}>{user.username}</Text>
+                    ) : (
+                        <Text style={{ color: theme ? 'grey' : 'white' }} onPress={getUser}>
+                            Press here to get your info!
+                        </Text>
+                    )}
+                </View>
+
+                <ProfileButton title='THEME' onPress={navigateTheme} />
+                <ProfileButton title='EDITPROFILE' onPress={navigateEditProfile} />
+                <ProfileButton title='LOGOUT' onPress={null} />
+
+            </View>
+        </View>
+    );
+}
+
+export default ProfileScreen;
---
+++ src/screen/ProfileStack/ProfileScreen/ProfileScreen.style.js
@@ -0,0 +1,18 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+    image: {
+        height: 100,
+        width: 100,
+        borderRadius: 50,
+    },
+    image_container: {
+        alignItems: 'center',
+        justifyContent: 'flex-start',
+        paddingVertical: 30,
+    },
+
+    username_container: {
+        flexDirection: 'row',
+    },
+});

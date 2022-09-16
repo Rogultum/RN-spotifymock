6b52a5b9c12ed1732d487bd1ab9db7782ff88ed1
Remzi Ogul TUM <amadeuwoles@gmail.com>
2022-09-17 02:28:23 +0300

    add firebase auth

--- src/screen/SignStack/SignInScreen/SignInScreen.js
+++ src/screen/SignStack/SignInScreen/SignInScreen.js
@@ -1,13 +1,17 @@
 import React, { useState } from 'react';
-import { Alert, SafeAreaView, ScrollView, Text, View } from 'react-native';
+import { Alert, SafeAreaView, ScrollView, Text, Pressable } from 'react-native';
 
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { useNavigation } from '@react-navigation/native';
 
+import { signInWithEmailAndPassword } from 'firebase/auth';
+
 import { useSelector } from 'react-redux';
+
 import SignButton from '../../../component/Button/SignButton'
 import Input from '../../../component/Input';
 import styles from './SignInScreen.style';
+import { auth } from '../../../../firebase/firebase';
 
 function SignIn() {
 
@@ -15,6 +19,7 @@ function SignIn() {
 
   const [userPassword, setUserPassword] = useState(null);
   const [username, setUsername] = useState(null);
+  const [userMail, setUserMail] = useState(null);
 
   const navigation = useNavigation();
 
@@ -24,6 +29,9 @@ function SignIn() {
     const user = JSON.parse(userData);
     if (user.username !== username) {
       return Alert.alert('Username not found!');
+    }
+    if (user.mail !== userMail) {
+      return Alert.alert('E-mail not found!');
     }
     if (user.password !== userPassword) {
       count += 1;
@@ -35,25 +43,33 @@ function SignIn() {
     if (count >= 3) {
       return Alert.alert('Do you want to reset your password?');
     }
+
+    signInWithEmailAndPassword(auth, userMail, userPassword)
+
     if (user.username === username && user.password === userPassword)
       return navigation.navigate('BottomNavigation');
 
     return null;
   };
 
+  const navigateSignUp = () => {
+    navigation.navigate('SignUp')
+  }
+
   return (
     <SafeAreaView style={[styles.container, { backgroundColor: theme ? 'white' : '#121212' }]}>
       <ScrollView>
         <Input label="Type your username" placeholder="username" onChangeText={setUsername} />
+        <Input label="Type your Email" placeholder="Email" onChangeText={setUserMail} />
         <Input
           label="Type your password"
           secureTextEntry
           placeholder="*****"
           onChangeText={setUserPassword}
         />
-        <View style={styles.question_container}>
+        <Pressable onPress={navigateSignUp} style={styles.question_container}>
           <Text style={styles.question_text}>Don&apos;t have an account?</Text>
-        </View>
+        </Pressable>
       </ScrollView>
       <SignButton title="Sign In" onPress={handleSignIn} />
     </SafeAreaView>

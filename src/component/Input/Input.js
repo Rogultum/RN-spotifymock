f80a071cd10545d5583a07d7f0669fbcd3c6d2c2
Remzi Ogul TUM <amadeuwoles@gmail.com>
2022-09-17 02:29:30 +0300

    add more functionalities and theme changes

--- src/component/Input/Input.js
+++ src/component/Input/Input.js
@@ -4,7 +4,7 @@ import { useSelector } from 'react-redux';
 
 import styles from './Input.style';
 
-function Input({ label, placeholder, onChangeText, secureTextEntry }) {
+function Input({ label, placeholder, onChangeText, secureTextEntry, value, onFocus, onBlur }) {
 
   const theme = useSelector((state) => state.theme.value);
 
@@ -13,10 +13,14 @@ function Input({ label, placeholder, onChangeText, secureTextEntry }) {
       <Text style={styles.label}>{label}</Text>
       <View style={[styles.input_container, { backgroundColor: theme ? 'white' : 'black' }]}>
         <TextInput
-          style={styles.text_input}
+          value={value}
+          style={[styles.text_input, { color: theme ? 'black' : 'white' }]}
           placeholder={placeholder}
           onChangeText={onChangeText}
           secureTextEntry={secureTextEntry}
+          placeholderTextColor={theme ? 'black' : 'white'}
+          onFocus={onFocus}
+          onBlur={onBlur}
         />
       </View>
     </View>

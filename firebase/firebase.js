bc3ad57e3a1da33de8e759a92cf6a5134db17b6b
Remzi Oğul TÜM <amadeuwoles@gmail.com>
2022-10-07 21:51:11 +0300

    Update firebase.js

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -5,13 +5,13 @@ import { getFirestore } from "firebase/firestore"
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBFzLLt9otiVnevs03H464qGCjkM5tgAic",
-  authDomain: "spotifymockrn.firebaseapp.com",
-  projectId: "spotifymockrn",
-  storageBucket: "spotifymockrn.appspot.com",
-  messagingSenderId: "360052113245",
-  appId: "1:360052113245:web:a3075acaceb09ecbe8b2b4",
-  measurementId: "G-LVCG7F4FBY",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "5",
+  appId: "",
+  measurementId: "",
 };
 
 // Initialize Firebase
